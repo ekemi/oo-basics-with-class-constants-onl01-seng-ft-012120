8df2189d6821fb3920988fe7b983cde3f84b4f25
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-05 17:48:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -17,4 +18,21 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+=======
+class Shoe
+  attr_accessor :color, :size, :material, :condition,:genre
+  attr_reader :brand
+
+GENRES=[]
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+
+>>>>>>> 6aa3b8a9e11836ec8e1063ea072a96d1692b37e2
 end
